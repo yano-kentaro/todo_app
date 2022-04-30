7794effb68c8f75c838b54108e3f9ce3de649412
yano-kentaro <ynkn0829@gmail.com>
2022-04-30 23:22:34 +0900

    main処理

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yano-kentaro/todo_app/config"
+)
+
+func main() {
+	fmt.Print("================================出力結果================================\n\n")
+
+	fmt.Println(config.Config)
+
+	fmt.Print("\n========================================================================\n")
+}
